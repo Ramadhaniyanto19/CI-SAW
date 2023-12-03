91479a2401b890033c72bdfb5d95877be771d173
Ramadhaniyanto19 <mramadhaniyanto@gmail.com>
2023-12-03 07:31:14 +0700

    My First Commit

---
+++ application/views/home.php
@@ -0,0 +1,14 @@
+<?php $this->load->view('template/header'); ?>
+
+<div class="box box-success">
+    <div class="box-header with-border">
+        <h3 class="box-title">Sistem Pendukung Keputusan</h3>
+    </div>
+    <div class="box-body" style="z-index:99;">
+        <p> Menentukan Judul Skripsi<br>
+         Menggunakan Metode Simple Additive Weighting Pada Universitas Negeri Indonesia</p>
+    </div>
+
+ <img src="<?php echo base_url('assets/images/homepng.jpg') ?>"  style="width: 100%; height: 620px; margin-top: -20px; z-index: -99;" class="d-block w-100" alt="...">
+</div>
+<?php $this->load->view('template/footer'); ?>
